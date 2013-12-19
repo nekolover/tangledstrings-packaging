f5046a6eb98bd8bba7ca27dd0162d0e00913caad
Jonathan Raffre <jonathan.raffre@smile.fr>
2013-12-19 13:59:51 +0100

    updating for RHEL/DEB

--- package.sh
+++ package.sh
@@ -3,16 +3,25 @@
 VERSION=1.0.3
 URL=https://github.com/nekolover/tangledstrings/archive/master.tar.gz
 
-# Downloading
-mkdir -p build usr/share/vim/vim73
+if [[ "${1}" == '-r' ]] ; then
+    VIMPATH=usr/share/vim/vimfiles
+    TARGET=rpm
+    VIMDEP=vim-enhanced
+else
+    VIMPATH=usr/share/vim/vim73
+    TARGET=deb
+    VIMDEP=vim
+fi
+
+rm -rf build usr && mkdir -p build ${VIMPATH}
 cd build
 wget ${URL} -O tangledstrings-${VERSION}.tar.gz
 tar xf tangledstrings-${VERSION}.tar.gz
 cd tangledstrings-master
 
 # Preparing 
-cp -r doc plugin ../../usr/share/vim/vim73
+cp -r doc plugin ../../${VIMPATH}
 cd ../../..
 
-fpm -n vim-tangledstrings -v ${VERSION} -a all -C tangledstrings-packaging -m "<jonathan.raffre@smile.fr>" --description "Vim Puppet management detector" --url 'https://github.com/nekolover/tangledstrings' -t deb -d vim -d puppet -s dir usr
+fpm -n vim-tangledstrings -v ${VERSION} -a all -C tangledstrings-packaging -m "<jonathan.raffre@smile.fr>" --description "Vim Puppet management detector" --url 'https://github.com/nekolover/tangledstrings' -t ${TARGET} -d $VIMDEP -d puppet -s dir usr
 
